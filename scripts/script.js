c8806488ff6ba181001d03e0b9c57f8904ee60b8
RomainMaillot <32742569+RomainMaillot@users.noreply.github.com>
2018-12-16 14:30:44 +0100

    :sparkles: Add class button close

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,15 @@
 const $buttons = document.querySelectorAll('.js-button')
 const $menu = document.querySelector('.js-menu')
+const $closeButton = document.querySelectorAll('.js-button-close')
 
 for(const $button of $buttons)
 {
     const button = new Button($button)
+}
+
+for(const $button of $closeButton)
+{
+    const button = new ButtonClose($button,$menu)
 }
 
 menu = new Menu($menu)
