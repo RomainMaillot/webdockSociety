ca5b3738facdce0e1a3b7a2ec8b7b6ad90a2c086
GDBrK <paulmercie453@gmail.com>
2018-12-18 12:29:32 +0100

    :sparkles: Adding new page how to use

--- scripts/roms.js
+++ scripts/roms.js
@@ -1,3 +1,29 @@
 const $pages = document.querySelector('.js-pages')
 
 page = new Pages($pages)
+
+// For the menu
+
+const $buttons = document.querySelectorAll('.js-button')
+const $menu = document.querySelector('.js-menu')
+const $menuButton = document.querySelector('.btn-menu')
+const $instructionsOpen = document.querySelector('.instructions--open')
+const $instructions = document.querySelector('.js-instructions')
+const $closeButton = document.querySelectorAll('.js-button-close')
+
+for(const $button of $buttons)
+{
+    const button = new Button($button)
+}
+for(const $button of $closeButton)
+{
+    const button = new ButtonClose($button,$instructions)
+}
+
+$instructionsOpen.addEventListener(
+    'click',
+    () =>
+    {
+        $instructions.classList.toggle('open')
+    }
+)
